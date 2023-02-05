d45a3034447f75263423c7844c345697f3ef37f7
IvanTorresMia <ivantorresmia27@gmail.com>
2023-02-05 14:52:11 -0800

    About me and hero phase 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 import React, { useCallback, useMemo } from "react"
 import { Menu } from 'antd';
+import Hero from './Pages/Hero';
+import AboutMe from './Pages/AboutMe';
+import { faFacebook, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFile, faAddressBook } from '@fortawesome/free-regular-svg-icons'
+import { faCode, faBusinessTime } from "@fortawesome/free-solid-svg-icons"
 
 function App() {
   const getItem = useCallback((label, key, icon, children, type) => {
@@ -15,11 +21,11 @@ function App() {
 
   let items = useMemo(() => {
     let items = [
-      getItem("Get to know me", "about"),
-      getItem("Experience", "experience"),
-      getItem("Technologies I work with", "tech"),
-      getItem("Resume", "resume"),
-  
+      getItem("Get to know me", "about", <FontAwesomeIcon icon={faAddressBook} />),
+      getItem("Experience", "experience", <FontAwesomeIcon icon={faBusinessTime} />),
+      getItem("Technologies I work with", "tech", <FontAwesomeIcon icon={faCode} />),
+      getItem("Resume", "resume",   <FontAwesomeIcon icon={faFile} />),
+
     ]
     return items
   }, [getItem])
@@ -28,10 +34,17 @@ function App() {
 
   }
   return (
-    <div className="container">
-        <div className='sider-menu'>
-        <div className='header font-xl p-m main-bc'>
-          Ivan Codes
+    <div className="container main-bc">
+      <div className='sider-menu'>
+        <div className='header font-xl p-m main-bc d-flex justify-content-between'>
+          <div className='text-color-w'>
+            Ivan Codes
+          </div>
+          <div className='d-flex justify-content-between' style={{ width: "100px"}}>
+            <FontAwesomeIcon icon={faFacebook} />
+            <FontAwesomeIcon icon={faGithub} />
+            <FontAwesomeIcon icon={faInstagram} />
+          </div>
         </div>
         <Menu
           onClick={handleNavClick}
@@ -39,7 +52,7 @@ function App() {
             width: 256,
             height: 500
           }}
-          className="navigation"
+          className="navigation main-bc"
           theme="dark"
           defaultSelectedKeys={['1']}
           defaultOpenKeys={['sub1']}
@@ -47,6 +60,8 @@ function App() {
           items={items}
         />
       </div>
+      <Hero />
+      <AboutMe />
     </div>
   );
 }
--- src/Pages/Hero.js
+++ src/Pages/Hero.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Typography } from "antd";
+let { Text, Title } = Typography;
 
 const Hero = () => {
 
     return (
-        <div>
-            
+        <div className="h-vh main-bc d-flex justify-content-center flex-column align-items-center">
+            <Title className="text-color-w">
+                Ivan Torres
+            </Title>
+            <Text className="text-color-w secondary-c font-xl">
+                Sofware engineer aka googling expert
+            </Text>
         </div>
     )
 }
